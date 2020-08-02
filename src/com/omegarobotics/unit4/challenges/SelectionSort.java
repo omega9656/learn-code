dd21241ed6f82a8516a32e961f17a0da2cf882a0
phrdang <phrdang@users.noreply.github.com>
2020-08-02 02:49:33 +0000

    Bot: Prettified Java code!

--- src/com/omegarobotics/unit4/challenges/SelectionSort.java
+++ src/com/omegarobotics/unit4/challenges/SelectionSort.java
@@ -10,12 +10,13 @@ Print the unsorted array and then the sorted array.
 */
 
 public class SelectionSort {
+
     public static void main(String[] args) {
         // unsorted array
-        int[] numbers = {5, 23, -25, 0, -14, 100, 34};
+        int[] numbers = { 5, 23, -25, 0, -14, 100, 34 };
 
         // print unsorted array
-        for (int n: numbers) {
+        for (int n : numbers) {
             System.out.print(n + " ");
         }
         System.out.println();
@@ -24,7 +25,7 @@ public class SelectionSort {
         sort(numbers);
 
         // print sorted array
-        for (int n: numbers) {
+        for (int n : numbers) {
             System.out.print(n + " ");
         }
         System.out.println();
@@ -36,27 +37,27 @@ public class SelectionSort {
      * @param list  array of integers to sort
      */
     public static void sort(int[] list) {
-        // list.length - 1 because don't need to check last element 
-		// since it will be sorted already
-		for (int i = 0; i < list.length - 1; i++) {		
-			int min = list[i];
-			int minIndex = i;
-			
-			// find the smallest element
-			for (int j = i + 1; j < list.length; j++) {
-				if (list[j] < min) {
-					min = list[j];
-					minIndex = j;
-				}
-			}
-			
-			// swap it with whatever is in the 
-			// first position of unsorted array if needed
-			if (minIndex != i) {
-				int temp = list[i];
-				list[i] = min;
-				list[minIndex] = temp;
-			}
-		}	
+        // list.length - 1 because don't need to check last element
+        // since it will be sorted already
+        for (int i = 0; i < list.length - 1; i++) {
+            int min = list[i];
+            int minIndex = i;
+
+            // find the smallest element
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[j] < min) {
+                    min = list[j];
+                    minIndex = j;
+                }
+            }
+
+            // swap it with whatever is in the
+            // first position of unsorted array if needed
+            if (minIndex != i) {
+                int temp = list[i];
+                list[i] = min;
+                list[minIndex] = temp;
+            }
+        }
     }
 }
