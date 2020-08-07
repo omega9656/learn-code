d891867e8a51dfcc70e1b4ddf1ad86f2149551cc
phrdang <phrdang@users.noreply.github.com>
2020-08-07 23:13:33 +0000

    Bot: Prettified Java code!

--- src/com/omegarobotics/unit7/lessons/TestCircle.java
+++ src/com/omegarobotics/unit7/lessons/TestCircle.java
@@ -1,6 +1,7 @@
 package com.omegarobotics.unit7.lessons;
 
 public class TestCircle {
+
     public static void main(String[] args) {
         Circle myCircle = new Circle(5);
         myCircle.setRadius(-5);
